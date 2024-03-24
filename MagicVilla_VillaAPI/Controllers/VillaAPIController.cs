8d3df75ba0edb37dc6bdabefa509d43dc08a85cb
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-03-24 09:32:24 -0400

    15. Using Controller Name in Route

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    //Can use the generic below to automatically route to Controller prefix:
+    //not ideal if you need to change controller name
+    //[Route("api/[controller]")] is the same as [Route("api/VillaAPI")] as It's in the VillaAPIController
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
