195e13992f81801104387641132367719655417e
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-03-26 10:50:32 -0400

    19. Status Code in EndPoints, define return type

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -13,15 +13,29 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList); //200 success
         }
 
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO>  GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+            //19. add validation for bad request 400
+            if (id == 0)
+            {
+                return BadRequest(); //400
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            //19. add validation for notfound 404
+            if (villa == null)
+            {
+                return NotFound(); //404
+            }
+
+            return Ok(villa);
         }
     }
 }
