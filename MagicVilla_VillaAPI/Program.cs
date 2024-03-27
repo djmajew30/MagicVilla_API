1de9553535869e810d905430d5332c47f03df645
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-03-27 16:07:39 -0400

    30. Content Negotiations

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -3,7 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //added in 27. patch nuget packages
-builder.Services.AddControllers().AddNewtonsoftJson();
+//builder.Services.AddControllers().AddNewtonsoftJson();
+//30. content negotiations
+builder.Services.AddControllers(option => {
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
