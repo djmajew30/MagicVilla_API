a78b465c0fb1ddbe1cb6e5763365dfa2c762e2a8
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-02 16:20:49 -0400

    51. Repository and IRepositoty, create generic- NEEDED when using multiple repository, exclude this if only 1 repo like the master list api

---
+++ MagicVilla_VillaAPI/Repository/Repository.cs
@@ -0,0 +1,71 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepostiory;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    //using T instead of Villa for generic
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        //add applicationDBContext
+        private readonly ApplicationDbContext _db; //ctrl + . generate constructor ctor
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        //add async to method
+        public async Task CreateAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet; //does not get executed right away, so we can add filter
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+
+    }
+}
