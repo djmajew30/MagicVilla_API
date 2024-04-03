a7883ddb5859d2bafb0214bd69e4d277a83a8cef
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-03 15:53:27 -0400

    52. update MagicVilla_VillaAPI/Models/APIResponse.cs

--- MagicVilla_VillaAPI/Models/APIResponse.cs
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -7,7 +7,7 @@ namespace MagicVilla_VillaAPI.Models
         //all responses can use all of these
         //i.e. 201, 204, 404, etc
         public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
 
         public object Result { get; set; }
