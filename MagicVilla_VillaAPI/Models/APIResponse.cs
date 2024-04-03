1f2d86ddfea508bea76d97a0bd3a8adf27dc634f
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-03 15:52:42 -0400

    52. API Response, typically 1 standard response

---
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class APIResponse
+    {
+        //all responses can use all of these
+        //i.e. 201, 204, 404, etc
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; }
+
+        public object Result { get; set; }
+    }
+}
