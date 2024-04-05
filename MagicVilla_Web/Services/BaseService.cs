a51cd0d1ef1d57a36b303441b672a3c69b2cfd43
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-05 10:50:54 -0400

    62. Base Service for API request and response. IBaseService and BaseService

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,79 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        //to call the api
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService()
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                //create client
+                var client = httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                //configure message (assign properties)
+                message.Headers.Add("Accept", "application/json");
+                //url where we call the api
+                message.RequestUri = new Uri(apiRequest.Url);
+                //serialize data if it exists
+                //data will not be null in POST/PUT http calls
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+                //define http type call
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                //when above sent, we will receive a response. set null by default
+                HttpResponseMessage apiResponse = null;
+                //call api endpoint
+                apiResponse = await client.SendAsync(message);
+                //get api content from response
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                //deserialize. then will be model apiresponse. has to be of type <T>
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch (Exception e)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+        }
+    }
+}
