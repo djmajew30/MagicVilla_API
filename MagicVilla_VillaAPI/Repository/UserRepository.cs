1ff05da50d8827519efd3a0f4e273249ed317904
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-11 17:20:39 -0400

    86. Add UserRepository and IUserRepository. has methods to be implemented 87. Implement User Repository - Register

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepostiory;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+
+            if (user == null) //user not found
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequestDTO registerationRequestDTO)
+        {
+            //add new user
+            LocalUser user = new()
+            {
+                UserName = registerationRequestDTO.UserName,
+                Password = registerationRequestDTO.Password,
+                Name = registerationRequestDTO.Name,
+                Role = registerationRequestDTO.Role
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            //clear password before sending response
+            user.Password = "";
+            return user;
+            
+        }
+    }
+}
