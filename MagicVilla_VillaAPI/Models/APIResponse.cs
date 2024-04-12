d19b2cfe5760c2bde0b853907a0a2e0a61f9afa6
djmajew30 <96849762+djmajew30@users.noreply.github.com>
2024-04-12 16:12:03 -0400

    91. Login and Register in Action, https://jwt.io/

--- MagicVilla_VillaAPI/Models/APIResponse.cs
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -6,6 +6,11 @@ namespace MagicVilla_VillaAPI.Models
     {
         //all responses can use all of these
         //i.e. 201, 204, 404, etc
+
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
